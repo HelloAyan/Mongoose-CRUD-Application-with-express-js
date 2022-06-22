52f1df7f3d8c13c3084ddc18cc56b7561b3f8a53
HelloAyan <18103373@iubat.edu>
2022-06-22 17:49:21 +0600

    connected with mongoose

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+// express app initialization
+const app = express();
+app.use(express.json());
+
+// database connection with mongoose
+mongoose.connect("mongodb://localhost/todos")
+    .then( ()=>{
+        console.log("Connection successful");
+    })
+    .catch((err) =>{
+        console.log(err);
+    })
+
+// Application routes
+
+
+// default error handler
+function errorHandler(err, req, res, next){
+    if(res.errorHandler){
+        return next(err);
+    }
+    res.status(500).json({err: err});
+}
+
+
+app.listen(3000, () =>{
+    console.log("App listening at port 3000");
+})
