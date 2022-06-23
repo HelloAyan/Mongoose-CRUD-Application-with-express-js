deb999ae2b26629b18607825d1c3eaa0e5430d21
HelloAyan <18103373@iubat.edu>
2022-06-23 12:34:25 +0600

    updated routerhandler

--- routerHandler/todoHandler.js
+++ routerHandler/todoHandler.js
@@ -6,3 +6,31 @@ const router = express.Router();
 router.get('/', async(req, res) =>{
     
 })
+
+// get A todo by ID
+router.get('/:id', async(req, res) =>{
+
+})
+
+
+// Post todo
+router.post('/', async(req, res)=>{
+
+})
+
+// post multiple todo
+router.post('/all', async(req, res)=>{
+
+})
+
+// put todo
+router.put("/:id", async(req, res)=>{
+
+})
+
+// Delete todo
+router.delete('/:id', async(req, res)=>{
+
+})
+
+module.exports=router;
